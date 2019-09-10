67b1e669fc3af1655da9f17bf8c029925c7566cd
Fahad <fahad011686@gmail.com>
2019-09-10 15:35:46 -0500

    Got js working better, now words fill in as user guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,56 +10,83 @@ var lib = [
 
 
 // randomizes a word from list
-randomizer = Math.floor(Math.random() * 5);
+randomizer = Math.floor(Math.random() * lib.length);
 currentWord = lib[randomizer];
 console.log(currentWord);
+
+
+
+
 // turns word into dashes
 
 
 var dashes = "";
 for (i = 0; i < currentWord.length - 1; i++) {
-    dashes += "_ ";
+    dashes += "_";
 }
 dashes += "_";
 // console.log(dashes);
 // return = dashes;
-// displays dashed word on HTML
 
+// displays dashed word on HTML
 var rando = document.querySelector("#randomWord");
 rando.innerHTML = dashes;
 // console.log(rando);
 
+
+
+
+
+// break currentWord and dashes into arrays
+var wordSplit = [];
+var dashSplit = [];
+wordSplit = currentWord.split('');
+dashSplit = dashes.split('');
+
 var guessRemaining = 10;
 
+var guess = true;
+console.log(guess);
 
 
 document.onkeyup = function (event) {
-    var letter = event.key.toUpperCase();
-    // console.log(letter);
+    var letter = event.key.toLowerCase();
+    console.log(letter);
     var change = document.querySelector("#letterGuessed");
-    change.innerHTML = letter;
+    change.innerHTML = letter.toUpperCase();
     // play sound effect
     document.getElementById('audio').play();
-
-    // break currentWord and dashes into arrays
-    var wordSplit = [];
-    var dashSplit = [];
-    wordSplit = currentWord.split('');
-    dashSplit = dashes.split('');
 
     // check if letter guessed is in currentWord
     for (i = 0; i < currentWord.length; i++) {
         if (letter == wordSplit[i]) {
             dashSplit[i] = letter;
-            // console.log(dashSplit);
+            console.log(dashSplit + '');
+            guess = true;
+
         }
         else {
-            guessRemaining--;
-            var score = document.querySelector("#guessRemaining");
-            score.innerHTML = guessRemaining;
+            guess = false;
+            console.log(guess);
         }
+
     }
 
     // update dashes to letter guessed correctly
     document.getElementById("randomWord").innerHTML = dashSplit.join("");
+
+    gCheck();
+
 }
+
+function gCheck() {
+    if (guess == false) {
+        guessRemaining--;
+    }
+
+var score = document.querySelector("#guessRemaining");
+        score.innerHTML = guessRemaining;
+guess = true;
+console.log(guess);
+};
+
